6910d180b24dc3c668d340be0dc7bf9fabd781cb
Michael Färber <01mf02@gmail.com>
2014-12-22 19:23:56 +0100

    Commit work on proof.

--- 10.thy
+++ 10.thy
@@ -2,32 +2,22 @@ theory 10
 imports Main
 begin
 
-(* iterate f n x calculates f\<^sup>n(x), that is, f applied n times to x *)
-(* "iterate f n x" applies f n times to x *)
-fun iterate :: "('a \<Rightarrow> 'a) \<Rightarrow> nat \<Rightarrow> 'a \<Rightarrow> 'a"
-where
-  "iterate f 0       x = x"
-| "iterate f (Suc n) x = f (iterate f n x)"
-
 fun h :: "nat \<Rightarrow> nat \<Rightarrow> nat"
 where
   "h 0       x = x + 1"
-| "h (Suc n) x = iterate (h n) x x"
+| "h (Suc n) x = (h n ^^ x) x"
 
-lemma h0_iterate: "iterate (h 0) n x = n + x"
-proof (induct n)
-  case 0 show ?case by auto
-  case (Suc n) then show ?case by auto
-qed
+lemma h0_compow: "(h 0 ^^ n) x = n + x"
+by (induct n, auto)
 
 lemma h1: "h 1 x = 2 * x"
-by (auto simp add: h0_iterate)
+by (auto simp add: h0_compow)
 
-lemma h1_iterate: "iterate (h 1) n x = x * 2^n"
-by (induct n, auto simp add: h0_iterate)
+lemma h1_compow: "(h 1 ^^ n) x = x * 2^n"
+by (induct n, auto simp add: h0_compow)
 
 theorem h2: "h 2 x = x * 2^x"
-using h1_iterate h.simps(2)[of 1] unfolding Suc_1 by auto
+using h1_compow h.simps(2)[of 1] unfolding Suc_1 by auto
 
 
 lemma hn1: "h n 1 = 2"
@@ -39,34 +29,35 @@ sorry
 lemma h_output_geq_input: "h n x \<ge> x"
 sorry
 
-
-lemma "iterate (h n) i x \<ge> x"
+lemma "(h n ^^ i) x \<ge> x"
 proof (induct i)
-  case 0 show ?case by simp
   case (Suc i) thus ?case using h_output_geq_input[of x n]
-                                h_monotonic_second[of x "iterate (h n) i x" n] by auto
+                                h_monotonic_second[of x "(h n ^^ i) x" n] by auto
+qed (simp)
+
+lemma h_monotonic_first_suc: "h (Suc n) (Suc x) \<ge> h n (Suc x)"
+proof (induct n)
+  case 0
+    have "h 0 (Suc x) \<le> h 1 (Suc x)" unfolding h1 by auto
+    then show ?case by simp
+next
+  case (Suc n) then show ?case sorry
 qed
 
 theorem h_monotonic_first:
   assumes "n \<ge> m"
   shows "h n (Suc x) \<ge> h m (Suc x)" using assms
 proof (induct n arbitrary: m)
-  case 0 then show ?case by auto
   case (Suc n)
-    fix n m m'
-    assume A1: "m' \<le> n \<Longrightarrow> h m' (Suc x) \<le> h n (Suc x)"
-    assume A2: "m \<le> Suc n"
-oops
-
-(*theorem h_monotonic_first: "n \<ge> m \<Longrightarrow> h n (Suc x) \<ge> h m (Suc x)"
-proof (induct x)
-  case 0 show ?case using hn1 by auto
-  case (Suc x)
-    then have IH1: "h m (Suc x) \<le> h n (Suc x)" by simp
-    show ?case using Suc.prems proof (induct n)
-      print_cases
-      case 0 then show ?case by auto
-      case (Suc n) 
-oops*)
+    then show ?case
+    proof (cases "m \<le> n")
+      case False
+        then have "m = Suc n" using Suc.prems by arith
+        thus ?thesis using Suc(1) by auto
+    next
+      case True
+        show ?thesis using Suc.hyps[OF True] using h_monotonic_first_suc[of n x] by auto
+    qed
+qed (auto)
 
 end
